1a99cf1f955e74b0eb66c14a4fac2894d8b29ec4
onlythompson <onlythompson@gmail.com>
2024-07-12 18:00:08 -0700

    Project Setup

---
+++ tis_fms_domain/IAggregateRoot.cs
@@ -0,0 +1,11 @@
+﻿namespace tis_fms_domain;
+
+/// <summary>
+/// Represents the base interface for an aggregate root in a domain-driven design.
+/// An aggregate root is the entry point to an aggregate, a cluster of domain entities and value objects
+/// that are treated as a single unit for the purpose of data changes.
+/// </summary>
+public interface AggregateRoot
+{
+
+}
---
+++ tis_fms_domain/shared_kernel/Currency.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tis_fms_domain.shared_kernel
+{
+    public class Currency
+    {
+        
+    }
+}
